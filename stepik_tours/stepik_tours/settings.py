f0d8b462905a374d19939ad92d5ef08c93b79553
Romashov Aleksey <amromashov@gmail.com>
2020-05-01 01:35:43 +0300

    New templates

--- stepik_tours/stepik_tours/settings.py
+++ stepik_tours/stepik_tours/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b%56ma)a)9xjwo!2&e_bz@ffcuz*v4m2s*yyujzbh+oct&+-b0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'stepik_tours.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- stepik_tours/tours/views.py
+++ stepik_tours/tours/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render
 from django.views import View
 from tours.data import title, subtitle, description, departures, tours
 from django.http import Http404
+import random
 
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
+        random_six_tours = random.sample(tours.items(), 6)
         return render(
-            request, 'tours/index.html', context={
-                'title': title,
-                'subtitle': subtitle,
-                'description': description,
-                'departures': departures,
-                'tours': tours,
+            request, 'index.html', context={
+                'site_title': title,
+                'site_subtitle': subtitle,
+                'site_description': description,
+                'menu': departures,
+                'tours': random_six_tours,
             }
         )
 
@@ -21,9 +23,9 @@ class DepartureView(View):
     def get(self, request, departure, *args, **kwargs):
         if departure in departures:
             return render(
-                request, 'tours/departure.html', context={
-                    'title': title,
-                    'departures': departures,
+                request, 'departure.html', context={
+                    'site_title': title,
+                    'menu': departures,
                     'departure': departures[departure],
                 }
             )
@@ -35,9 +37,17 @@ class TourView(View):
     def get(self, request, id, *args, **kwargs):
         if id in tours:
             return render(
-                request, 'tours/tour.html', context={
-                    'title': title,
-                    'departures': departures,
+                request, 'tour.html', context={
+                    'site_title': title,
+                    'menu': departures,
+                    'tour_title': tours[id]['title'],
+                    'starts': tours[id]['stars'],
+                    'tour_country': tours[id]['country'],
+                    'departure': departures[tours[id]['departure']],
+                    'nights': tours[id]['nights'],
+                    'tour_description': tours[id]['description'],
+                    'picture': tours[id]['picture'],
+                    'price': tours[id]['price'],
                 }
             )
         else:
